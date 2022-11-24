b294fe545b6afbfaf01bb1fb8a48becf238d9a34
Valisaurus <valley.alosachi@hotmail.com>
2022-11-24 17:23:20 +0100

    hamburger-menu

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-// const menu = document.querySelector(".menu");
-// const menuItems = document.querySelectorAll(".menuItem");
-// const hamburger= document.querySelector(".hamburger");
-// const closeIcon= document.querySelector(".closeIcon");
-// const menuIcon = document.querySelector(".menuIcon");
+/* THIS PART IS FETCH FROM https://www.youtube.com/watch?v=flItyHiDm7E  AND MODIFIED BY VALI */
 
-// function toggleMenu() {
-//   if (menu.classList.contains("showMenu")) {
-//     menu.classList.remove("showMenu");
-//     closeIcon.style.display = "none";
-//     menuIcon.style.display = "block";
-//   } else {
-//     menu.classList.add("showMenu");
-//     closeIcon.style.display = "block";
-//     menuIcon.style.display = "none";
-//   }
-// }
+const hamburgerLines = document.querySelector(".hamburger-lines");
 
-// hamburger.addEventListener("click", toggleMenu);
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+
+hamburgerLines.addEventListener("click", () => {
+  hamburgerLines.classList.toggle("active");
+  hamburgerMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".hamburger-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburgerLines.classList.remove("active");
+    hamburgerMenu.classList.remove("active");
+  })
+);
