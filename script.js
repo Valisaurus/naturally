ea3a9b8d4b2e79afd259c6422ec1110b6c3d28a7
AnnaBrum <annahelenabrumark@gmail.com>
2022-11-29 21:41:11 +0100

    no changes

--- script.js
+++ script.js
@@ -18,15 +18,30 @@ document.querySelectorAll(".hamburger-link").forEach((n) =>
 
 /* SEARCHBAR */
 
-// const searchBar = document.querySelector(".search-bar");
-// const searchIcon = document.querySelector(".nav-item-search");
-// const exitButton = document.querySelector(".exit-button");
+const searchBar = document.querySelector(".search-bar");
+const searchIcon = document.querySelector(".nav-item-search");
+const exitButton = document.querySelector(".exit-button");
 
-// searchIcon.addEventListener("click", () => {
-//   searchBar.classList.toggle("active");
-// });
+searchIcon.addEventListener("click", () => {
+  searchBar.classList.toggle("active");
+});
 
 /* IMAGE ANIMATION  */
 
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal")
 
-const 
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
