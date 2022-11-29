8f628c8fb1aedc73894224994e1296abf9ab7c80
AnnaBrum <annahelenabrumark@gmail.com>
2022-11-29 20:19:11 +0100

    Added image-animation, changed footer image

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.querySelectorAll(".hamburger-link").forEach((n) =>
   })
 );
 
-// SEARCHBAR
+/* SEARCHBAR */
 
 const searchBar = document.querySelector(".search-bar");
 const searchIcon = document.querySelector(".nav-item-search");
@@ -25,3 +25,23 @@ const exitButton = document.querySelector(".exit-button");
 searchIcon.addEventListener("click", () => {
   searchBar.classList.toggle("active");
 });
+
+/* IMAGE ANIMATION */
+
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal")
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
