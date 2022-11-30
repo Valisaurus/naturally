b7114e2f3fb47ae028dfd6eacd5749fe3b3fce1b
Valisaurus <valley.alosachi@hotmail.com>
2022-11-30 16:17:57 +0100

    meny, movie

--- script.js
+++ script.js
@@ -1,3 +1,62 @@
+/*ARRAY WITH MP4-VIDEOS */
+const videos = [
+  {
+    name: "kaftan",
+    video: "./videos/kaftananimation.512x560.mp4",
+  },
+  {
+    name: "slippers",
+    video: "./videos/Slipper_Anim_512x344.mp4",
+  },
+];
+/* CALLING FOR THE IMAGES AND THEN ADDING A VIDEO WHEN CLICKING ON THE IMAGE */
+// const kaftanDiv = document.querySelector(".kaftan");
+// const video = document.querySelector("kaftan-video");
+
+// kaftanDiv.addEventListener("click", () => {
+//   video.classList.toggle("active");
+// });
+
+// kaftanDiv.addEventListener("click", () => {
+//   kaftanVideo.classList.remove("active");
+// });
+
+// const kaftanDiv = document.querySelector(".kaftan");
+// const imgKaftan = document.getElementById("kaftan-img");
+
+// const addVideo = () => {
+//   const video = document.createElement("video");
+//   video.src = videos[0].video;
+// };
+// const addedVideo = addVideo();
+
+// imgKaftan.addEventListener("click", () => {
+//   imgKaftan.addedVideo = addedVideo;
+// });
+
+// const slippersImg = document.querySelector(".slippers");
+// const kaftanImg = document.querySelector(".kaftan-img");
+// const kaftanDiv = document.querySelector(".kaftan");
+
+// kaftanImg.addEventListener("click", () => {
+//   // const video = document.createElement("video");
+//   // video.src = videos[0].video;
+//   // video.controls = true;
+//   // video.muted = false;
+//   // video.height = 240;
+//   // video.width = 320;
+//   // toggle = !toggle;
+//   // if (toggle) {
+//   kaftanDiv.innerHTML = `<video width="100%" height="100%" autoplay="autoplay" loop>
+//     <source src="./videos/kaftananimation.512x560.mp4" type="video/mp4" />
+//     </video> <p>GINKGO KAFTAN</p>`;
+//   // }
+
+//   kaftanImg.classList.remove("active");
+
+//   kaftanImg.classList.toggle("active");
+// });
+
 /* THIS PART IS FETCH FROM https://www.youtube.com/watch?v=flItyHiDm7E  AND MODIFIED BY VALI */
 
 const hamburgerLines = document.querySelector(".hamburger-lines");
@@ -15,6 +74,8 @@ document.querySelectorAll(".hamburger-link").forEach((n) =>
     hamburgerMenu.classList.remove("active");
   })
 );
+
+/*END OF FETCHED PART */
 
 /* SEARCHBAR */
 
