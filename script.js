3759f45bfc958dcc658e9633e38b8f1458867c6e
Valisaurus <valley.alosachi@hotmail.com>
2022-12-02 02:27:44 +0100

    FINAL ONE

--- script.js
+++ script.js
@@ -1,75 +1,4 @@
-/* CALLING FOR THE IMAGES AND THEN ADDING A VIDEO WHEN CLICKING ON THE IMAGE */
-
-// const kaftanDiv = document.querySelector(".kaftan");
-// const video = document.querySelector("kaftan-video");
-
-// kaftanDiv.addEventListener("click", () => {
-//   video.classList.toggle("active");
-// });
-
-// kaftanDiv.addEventListener("click", () => {
-//   kaftanVideo.classList.remove("active");
-// });
-
-// const kaftanDiv = document.querySelector(".kaftan");
-// const imgKaftan = document.getElementById("kaftan-img");
-
-// const addVideo = () => {
-//   const video = document.createElement("video");
-//   video.src = videos[0].video;
-// };
-// const addedVideo = addVideo();
-
-// imgKaftan.addEventListener("click", () => {
-//   imgKaftan.addedVideo = addedVideo;
-// });
-
-// const slippersImg = document.querySelector(".slippers");
-// const kaftanImg = document.querySelector(".kaftan-img");
-// const kaftanDiv = document.querySelector(".kaftan");
-
-// kaftanImg.addEventListener("click", () => {
-//   // const video = document.createElement("video");
-//   // video.src = videos[0].video;
-//   // video.controls = true;
-//   // video.muted = false;
-//   // video.height = 240;
-//   // video.width = 320;
-//   // toggle = !toggle;
-//   // if (toggle) {
-//   kaftanDiv.innerHTML = `<video width="100%" height="100%" autoplay="autoplay" loop>
-//     <source src="./videos/kaftananimation.512x560.mp4" type="video/mp4" />
-//     </video> <p>GINKGO KAFTAN</p>`;
-//   // }
-
-//   kaftanImg.classList.remove("active");
-
-//   kaftanImg.classList.toggle("active");
-// });
-
-// FETCHED FROM https://www.youtube.com/watch?v=ydeQbbF9jbw AND MODIFIED BY VALI
-
-// const kaftanImg = document.querySelector(".kaftan-img");
-// let toggle = true;
-// kaftanImg.addEventListener("click", () => {
-//   toggle = !toggle;
-//   if (toggle) {
-//     kaftanImg.src = "./images/kaftanbild.640x688.png";
-//   } else {
-//     kaftanImg.src = "./gifs/kaftananimation.640x688.gif";
-//   }
-// });
-
-// const slipperImg = document.querySelector(".slipper-img");
-
-// slipperImg.addEventListener("click", () => {
-//   toggle = !toggle;
-//   if (toggle) {
-//     slipperImg.src = "./images/Slipper_Final_640x432.jpg";
-//   } else {
-//     slipperImg.src = "./gifs/Slipper_Anim_640x432.gif";
-//   }
-// });
+//VIDEO ANIMATION
 
 const videoKaftan = document.querySelector(".kaftan video");
 const videoSrcKaftan = document.querySelector(".kaftan video source");
@@ -145,6 +74,8 @@ exitButton.addEventListener("click", () => {
 
 /* IMAGE ANIMATION  */
 
+// FETCHED FROM https://alvarotrigo.com/blog/css-animations-scroll/ AND MODIFIED BY ANNA
+
 function reveal() {
   const reveals = document.querySelectorAll(".reveal");
 
@@ -162,23 +93,3 @@ function reveal() {
 }
 
 window.addEventListener("scroll", reveal);
-
-// videoSlipper.addEventListener("mouseleave", loadVideoSlipper, () => {});
-// videoSlipper.addEventListener("mouseleave", videoSrcSlipper.pause());
-
-// window.addEventListener("resize", loadVideoKaftan);
-// window.addEventListener("resize", loadVideoSlipper);
-
-// const kaftanDiv = document.querySelector(".kaftan");
-
-// const kaftanImg = document.querySelector(".kaftan-img");
-// const kaftanGif = document.querySelector(".kaftan-gif");
-
-// kaftanDiv.addEventListener("click", () => {
-//   kaftanGif.classList.toggle("active");
-//   kaftanImg.classList.remove("active");
-// });
-
-// kaftanGif.addEventListener("click", () => {
-//   kaftanGif.classList.remove("active");
-// });
